3a9eec01faabb0ee7c713314f9f4da0096eaf4de
PreetamPawar-dev <pawarpreetam385@gmail.com>
2026-05-04 10:09:19 +0530

    Create Password-Checker.py

---
+++ Password-Checker.py
@@ -0,0 +1,8 @@
+password = "112233"
+while True:
+        enter = input("Enter Your Password: ")
+        if (enter == password):
+                print("Password Is Correct")
+                break
+        else:
+                print("Password Is Wrong")
